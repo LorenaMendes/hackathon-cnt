e987f4070e2fc2b17c00bba9960cbbeffd06313c
Thiago <thiagoxdg@gmail.com>
2020-10-24 18:35:42 -0300

    picker data

--- waysegapp/src/pages/researchPage/components/researchForm/ResearchForm.tsx
+++ waysegapp/src/pages/researchPage/components/researchForm/ResearchForm.tsx
@@ -1,12 +1,16 @@
 import { Formik } from 'formik'
 import React, { FC, useCallback, useState } from 'react'
-import { View, Text, TextInputProps, Button } from 'react-native'
+import { View, Text, TextInputProps, Button, TouchableNativeFeedback, Keyboard } from 'react-native'
 import InputItem from '../../../../components/inputTextItem/InputItem'
 import { IResearch } from '../../../../models/Research/IResearch'
 import styles from './ResearchFormStyle'
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import RNDateTimePicker from '@react-native-community/datetimepicker'
 
 const ResearchForm = () => {
     const [research, setResearch] = useState<IResearch>({} as IResearch)
+    const [date, setDate] = useState(new Date());
+    const [showDatePicker, setShowDatePicker] = useState(false)
 
     const onSubmitHandle = useCallback(
         () => {
@@ -21,9 +25,24 @@ const ResearchForm = () => {
         },
         [],
     )
+    const onChangeDateTime = useCallback(
+        (_date) => {
+            setDate(_date);
+            setShowDatePicker(false);
+
+        },
+        [],
+    )
+
+
     return (
         <View style={styles.container}>
-            <Text>ResearchForm</Text>
+            < DateTimePickerModal
+                isVisible={showDatePicker}
+                mode="date"
+                onConfirm={onChangeDateTime}
+                onCancel={() => { setShowDatePicker(false); }}
+            />
             <InputItem
                 label="Formulario"
                 textInputProps={
@@ -36,7 +55,36 @@ const ResearchForm = () => {
                 label="Folha"
                 textInputProps={{ value: research.roadName }}
             />
-            <Button title={"Enviar"} onPress={onSubmitHandle} ></Button>
+
+            <TouchableNativeFeedback
+                onPress={() => { setShowDatePicker(true); Keyboard.dismiss() }}
+            >
+                <View
+                    style={
+                        {
+                            padding: 12
+                        }
+                    }
+                >
+                    <Text style={{ marginBottom: 8 }} >Data:</Text>
+                    <Text
+                        style={
+                            {
+                                borderBottomWidth: 1,
+                                padding: 4,
+                                borderBottomColor: "#fa0"
+                            }
+                        }
+                    >
+                        {date.getDay()}/{date.getMonth()}/{date.getFullYear()}
+                    </Text>
+                </View>
+            </TouchableNativeFeedback>
+
+
+
+
+
         </View>
 
     )
