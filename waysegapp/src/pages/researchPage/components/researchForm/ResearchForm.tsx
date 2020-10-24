d8b731ff07ba207b2df6d470d07fcfd85d0fd93a
LorenaMendes <lorenamendes.contato@gmail.com>
2020-10-24 20:39:17 -0300

    removed s

--- waysegapp/src/pages/researchPage/components/researchForm/ResearchForm.tsx
+++ waysegapp/src/pages/researchPage/components/researchForm/ResearchForm.tsx
@@ -1,11 +1,12 @@
 import React, { FC, useCallback, useState } from 'react'
 import { View, Text, TextInputProps, TouchableNativeFeedback, Keyboard } from 'react-native'
 import InputItem from '../../../../components/inputTextItem/InputItem'
+import InputOption from '../../../../components/inputOption/InputOption'
 import { IResearch } from '../../../../models/Research/IResearch'
 import Button from '../../../../components/button/Button'
 import styles from './ResearchFormStyle'
 import DateTimePickerModal from "react-native-modal-datetime-picker";
-s
+
 const ResearchForm = () => {
     const [research, setResearch] = useState<IResearch>({} as IResearch)
     const [date, setDate] = useState(new Date());
@@ -35,58 +36,74 @@ const ResearchForm = () => {
 
 
     return (
-        <View style={styles.container}>
-            < DateTimePickerModal
-                isVisible={showDatePicker}
-                mode="date"
-                onConfirm={onChangeDateTime}
-                onCancel={() => { setShowDatePicker(false); }}
-            />
-            <InputItem
-                label="Formulario"
-                textInputProps={
-                    {
-                        value: research.form
-                    }
-                }
-            />
-            <InputItem
-                label="Folha"
-                textInputProps={{ value: research.roadName }}
-            />
+      <View style={styles.container}>
+        <DateTimePickerModal
+          isVisible={showDatePicker}
+          mode="date"
+          onConfirm={onChangeDateTime}
+          onCancel={() => {
+            setShowDatePicker(false);
+          }}
+        />
+        <InputItem
+          label="Formulario"
+          textInputProps={{
+            value: research.form,
+          }}
+        />
+        <InputItem label="Folha" textInputProps={{value: research.roadName}} />
 
-            <TouchableNativeFeedback
-                onPress={() => { setShowDatePicker(true); Keyboard.dismiss() }}
-            >
-                <View
-                    style={
-                        {
-                            padding: 12
-                        }
-                    }
-                >
-                    <Text style={{ marginBottom: 8 }} >Data:</Text>
-                    <Text
-                        style={
-                            {
-                                borderBottomWidth: 1,
-                                padding: 4,
-                                borderBottomColor: "#fa0"
-                            }
-                        }
-                    >
-                        {date.getDay()}/{date.getMonth()}/{date.getFullYear()}
-                    </Text>
-                </View>
-            </TouchableNativeFeedback>
+        <TouchableNativeFeedback
+          onPress={() => {
+            setShowDatePicker(true);
+            Keyboard.dismiss();
+          }}>
+          <View
+            style={{
+              padding: 12,
+            }}>
+            <Text style={{marginBottom: 8}}>Data:</Text>
+            <Text
+              style={{
+                borderBottomWidth: 1,
+                padding: 4,
+                borderBottomColor: '#fa0',
+              }}>
+              {date.getDate()}/{date.getMonth()}/{date.getFullYear()}
+            </Text>
+          </View>
+        </TouchableNativeFeedback>
 
+        <Text>Campo</Text>
+        <InputOption
+          value={0}
+          label="Pista dupla com canteiro central"
+          checked={false}
+        />
+        <InputOption
+          value={1}
+          label="Pista dupla com barreira central"
+          checked={true}
+        />
+        <InputOption
+          value={2}
+          label="Pista dupla com faixa central"
+          checked={false}
+        />
+        <InputOption
+          value={3}
+          label="Pista simples de mão única"
+          checked={false}
+        />
+        <InputOption
+          value={4}
+          label="Pista simples de mão dupla"
+          checked={false}
+        />
 
-
-
-
-        </View>
-
-    )
+        <Button title="Gerar Formulário" />
+      </View>
+    );
 }
 
 
