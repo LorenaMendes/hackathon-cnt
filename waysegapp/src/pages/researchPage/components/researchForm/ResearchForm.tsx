9ba119a686be2564c2d71a304437a435782cb863
LorenaMendes <lorenamendes.contato@gmail.com>
2020-10-24 18:37:24 -0300

    added call to button component

--- waysegapp/src/pages/researchPage/components/researchForm/ResearchForm.tsx
+++ waysegapp/src/pages/researchPage/components/researchForm/ResearchForm.tsx
@@ -1,8 +1,8 @@
-import { Formik } from 'formik'
 import React, { FC, useCallback, useState } from 'react'
-import { View, Text, TextInputProps, Button } from 'react-native'
+import { View, Text, TextInputProps } from 'react-native'
 import InputItem from '../../../../components/inputTextItem/InputItem'
 import { IResearch } from '../../../../models/Research/IResearch'
+import Button from '../../../../components/button/Button'
 import styles from './ResearchFormStyle'
 
 const ResearchForm = () => {
@@ -22,24 +22,19 @@ const ResearchForm = () => {
         [],
     )
     return (
-        <View style={styles.container}>
-            <Text>ResearchForm</Text>
-            <InputItem
-                label="Formulario"
-                textInputProps={
-                    {
-                        value: research.form
-                    }
-                }
-            />
-            <InputItem
-                label="Folha"
-                textInputProps={{ value: research.roadName }}
-            />
-            <Button title={"Enviar"} onPress={onSubmitHandle} ></Button>
-        </View>
-
-    )
+      <View style={styles.container}>
+        {/* <Text>ResearchForm</Text> */}
+        <InputItem
+          label="Formulario"
+          textInputProps={{
+            value: research.form,
+          }}
+        />
+        <InputItem label="Folha" textInputProps={{value: research.roadName}} />
+        {/* <Button title={'Enviar'} onPress={onSubmitHandle}></Button> */}
+        <Button title="Criar Formulário" />
+      </View>
+    );
 }
 
 
