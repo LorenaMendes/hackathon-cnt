9bc095a9c5f171c48e00f7e59037913c51e269fa
Thiago <thiagoxdg@gmail.com>
2020-10-24 10:48:21 -0300

    type script init

---
+++ waysegapp/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { SafeAreaView, Text, View } from 'react-native'
+import { styles } from './Style'
+
+const App = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text >
+                HELLO FUCKING WORLD
+            </Text>
+        </SafeAreaView>
+    )
+}
+
+export default App
+
+
